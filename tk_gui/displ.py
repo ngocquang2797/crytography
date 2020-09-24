a2748cf63bf2a7b815133672fbefada6b427906b
Quang Nguyen <ngocquang2797@gmail.com>
2020-09-24 23:55:11 +0700

    gui

--- tk_gui/displ.py
+++ tk_gui/displ.py
@@ -10,10 +10,19 @@ class Sha512_displ(Frame):
     def init_window(self):
         # allowing the widget to take the full space of the root window
         self.pack(fill=BOTH, expand=1)
-        
+        plain_txt = Label(self, text="Plaintext")
+        plain_txt.grid(row=0, column=1, padx=30, pady=30)
+        plain_txt_box = Entry(self, text="Plaintext")
+        plain_txt_box.grid(row=0, column=2)
+        code_txt = Label(self, text="XX")
+        code_txt.grid(row=1, column=1)
+        code_txt_box = Entry(self, text="Decode")
+        code_txt_box.insert(index=1,string="asdasdasd")
+        code_txt_box.configure(state="readonly")
+        code_txt_box.grid(row=1, column=2)
 
 
-    #
+#
 #
 #
 # root = Tk()
@@ -36,11 +45,11 @@ tab1 = ttk.Frame(tabControl)
 tab2 = Sha512_displ()
 # tab2 = ttk.Frame(tabControl)
 
-tabControl.add(tab1, text='Tab 1')
 tabControl.add(tab2, text='SHA-512')
+tabControl.add(tab1, text='Tab 1')
 tabControl.pack(expand=1, fill="both")
 
 ttk.Label(tab1, text="Welcome to GeeksForGeeks").grid(column=0, row=0, padx=30, pady=30)
-# ttk.Label(tab2).grid(column=0, row=0, padx=30, pady=30)
+
 
 root.mainloop()
